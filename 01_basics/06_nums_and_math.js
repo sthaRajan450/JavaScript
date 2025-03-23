af80cbfd9b89ab62d2d114e8767033d7e16f29de
rajan shrestha <stharajan477@gmail.com>
2025-03-23 22:19:49 +0545

    Math and Number

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,26 @@
+const score = 400;
+const balance = new Number(1000);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+const otherNumber = 23.65657;
+// console.log(otherNumber.toFixed(2));
+// console.log(otherNumber.toPrecision(2));
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+//++++++++++++++++++++ Maths ++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.min(334, 45, 6, 7, 34));
+// console.log(Math.round(6.3));
+
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(9.1));
+// console.log(Math.floor(Math.PI));
+
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+const min = 20;
+const max = 30;
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
