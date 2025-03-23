ae8508699b385199efc5c91641e292ddda183e76
rajan shrestha <stharajan477@gmail.com>
2025-03-23 18:42:41 +0545

    opertions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,7 +14,7 @@ let valueInNumber = Number(score);
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1=>true
 //0=>false
@@ -23,4 +23,28 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+//************operations *****************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Rajan";
+let str2 = " Shrestha";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
