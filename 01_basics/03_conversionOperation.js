fb9307a803950903311ca0a1d9e0b71959f87b8d
rajan shrestha <stharajan477@gmail.com>
2025-03-23 18:24:50 +0545

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = true;
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc"=>NaN
+//true=>1
+//false=>0
+//"raj"=>NaN
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1=>true
+//0=>false
+//""=>false
+//"Raj"=>true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
