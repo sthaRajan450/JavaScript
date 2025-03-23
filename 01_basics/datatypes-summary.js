0f8df9b8076783f4997eb83b066c5d2c7aec3ec8
rajan shrestha <stharajan477@gmail.com>
2025-03-23 21:16:04 +0545

    stack and heap memory concept

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -10,10 +10,10 @@ let userEmail;
 
 const id = Symbol("123");
 const anotherId = Symbol("123");
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 const bigNumber = 235463421324523n;
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
 //Reference(Non-primitive) (call by reference)
 //Array , Objects , Functions
@@ -22,7 +22,28 @@ const heros = ["hulk", "ironman", "thor"];
 let myObj = { name: "Rajan", age: 23, address: "Arghakhanchi" };
 
 const myFunc = function () {
-  console.log("Hello Rajan");
+  //   console.log("Hello Rajan");
 };
 
-console.log(typeof myFunc)
+// console.log(typeof myFunc)
+
+//********************************************** */
+//Stack (Primitive), Heap(Non primitive)
+let valu1 = 45;
+let value2 = valu1;
+value2++;
+console.log(valu1, value2);
+
+let array1 = ["Rajan", "manish", "nabin"];
+let array2 = array1;
+array2.push("pradip");
+console.log(array2, array1);
+
+let userOne = {
+  email: "rajan@google.com",
+  address: "palpa",
+};
+let userTwo = userOne;
+userTwo.email = "manish@google.com";
+console.log(userOne.email);
+console.log(userTwo.email);
