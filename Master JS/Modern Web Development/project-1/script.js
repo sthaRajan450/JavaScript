f61d3cd06a683a3cb696e44ffe9ea0288ad75d98
rajan shrestha <stharajan477@gmail.com>
2025-04-15 23:45:05 +0545

    some gsap features added

--- Master JS/Modern Web Development/project-1/script.js
+++ Master JS/Modern Web Development/project-1/script.js
@@ -10,4 +10,30 @@ function circleMouseFollower() {
     ).style.transform = `translate(${e.clientX}px,${e.clientY}px)`;
   });
 }
+
+function firstPageAnim() {
+  let t1 = gsap.timeline();
+  t1.from("#nav", {
+    y: -10,
+    opacity: 0,
+    duration: 1,
+    ease: Expo.easeInOut,
+  })
+    .to(".boundingelem", {
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 2,
+      delay: '-1',
+      stagger: 0.2,
+    })
+    .from("#herofooter", {
+      y: -10,
+      opacity: 0,
+      duration: 1,
+      delay:'-1',
+      ease: Expo.easeInOut,
+    });
+}
+
 circleMouseFollower();
+firstPageAnim();
