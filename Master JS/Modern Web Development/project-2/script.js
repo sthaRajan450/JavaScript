f8100aa18fcdc5298b384e1d56449bac446bf723
rajan shrestha <stharajan477@gmail.com>
2025-04-17 23:08:56 +0545

    mouse follower and magnet added

--- Master JS/Modern Web Development/project-2/script.js
+++ Master JS/Modern Web Development/project-2/script.js
@@ -18,3 +18,16 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
+
+Shery.mouseFollower({
+  //Parameters are optional.
+  skew: true,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration:0.5
+});
+
+Shery.makeMagnet(".magnet-target" /* Element to target.*/, {
+  //Parameters are optional.
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
